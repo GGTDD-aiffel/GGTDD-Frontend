3e73fe460631d7efcfc155940373c003635fa469
Dennis-HyeongHo-Park <ho128say@naver.com>
2025-03-17 13:35:03 +0900

    update widget_test.dart file.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ggtdd_frontend/ui/features/welcome/widgets/welcome_screen.dart';
 
-import 'package:ggtdd_frontend/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
