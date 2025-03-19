d11296abd6fd63b536a3e97e922f6202b9f027a3
DataCodeLiteracy <dataliteracy@icloud.com>
2025-03-19 11:35:18 +0900

    [ ⚙️ | #6 | env ] Flutter에서 파이어베이스 통신을 위한 초기 세팅
    2025.03.19

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:ggtdd_frontend/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'config/flavor_config.dart';
 import 'config/providers.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   FlavorConfig(
     flavor: Flavor.development,
     baseUrl: "http://0.0.0.0:8000",
